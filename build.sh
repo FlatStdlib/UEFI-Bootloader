3244ccbf3366ebe814949ae718fb074e59dd1e99
Algorithms <142917637+Algo1337@users.noreply.github.com>
2026-02-05 02:47:28 -0600

    internal, memory and allocator implementation

--- build.sh
+++ build.sh
@@ -1,29 +1,25 @@
-# x86_64-w64-mingw32-gcc -ffreestanding -fshort-wchar -mno-red-zone \
-#   -I third_party_libs/gnu-efi/inc \
-#   -c test.c -o test.obj
-
 mkdir -p third_party_libs
 
-x86_64-w64-mingw32-gcc -ffreestanding -fshort-wchar -mno-red-zone \
-  -I third_party_libs/gnu-efi/inc \
-  -c src/allocator.c -o allocator.obj
+# x86_64-w64-mingw32-gcc -ffreestanding -fshort-wchar -mno-red-zone \
+#   -I third_party_libs/gnu-efi/inc \
+#   -c src/allocator.c -o allocator.obj
 
-x86_64-w64-mingw32-gcc -ffreestanding -fshort-wchar -mno-red-zone \
-  -I third_party_libs/gnu-efi/inc \
-  -c src/init.c -o init.obj
+# x86_64-w64-mingw32-gcc -ffreestanding -fshort-wchar -mno-red-zone \
+#   -I third_party_libs/gnu-efi/inc \
+#   -c src/init.c -o init.obj
   
-x86_64-w64-mingw32-gcc -ffreestanding -fshort-wchar -mno-red-zone \
-  -I third_party_libs/gnu-efi/inc \
-  -c src/internal.c -o internal.obj
+# x86_64-w64-mingw32-gcc -ffreestanding -fshort-wchar -mno-red-zone \
+#   -I third_party_libs/gnu-efi/inc \
+#   -c src/internal.c -o internal.obj
 
+FILES=$(cd src && ls -1 *.c; cd ..)
+PWD=$(pwd)
+for file in $FILES; do 
+  x86_64-w64-mingw32-gcc -ffreestanding -fshort-wchar -mno-red-zone \
+    -I third_party_libs/gnu-efi/inc \
+    -c "src/$file" -o "${file%.c}.obj"
+done
 nasm -f win64 boot.asm -o boot.obj
-
-# lld-link \
-#   /subsystem:efi_application \
-#   /entry:efi_main \
-#   /nodefaultlib \
-#   /out:BOOTX64.EFI \
-#   boot.obj
 
 
 lld-link \
@@ -33,7 +29,6 @@ lld-link \
   /out:BOOTX64.EFI \
   *.obj
 
-# rm boot.obj test.obj BOOTX64.lib
 rm *.obj BOOTX64.lib
 
 download_efi() {
